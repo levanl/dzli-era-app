846e871d608643902463464b4701834f4051de3c
Levan Loladze <levan.loladzew@gmail.com>
2024-01-19 17:47:11 +0400

    Make Sign In Page Visual Aspect and Extract reusable components

--- DzliEra/DzliEra/Scenes/Authentication/Sign In/SignInEmailView.swift
+++ DzliEra/DzliEra/Scenes/Authentication/Sign In/SignInEmailView.swift
@@ -14,18 +14,29 @@ struct SignInEmailView: View, WithRootNavigationController {
     @StateObject private var viewModel = SignInEmailViewModel()
     
     var body: some View {
+        
         VStack {
-            TextField("Email...", text: $viewModel.email)
-                .padding()
-                .background(Color.gray.opacity(0.4))
-                .cornerRadius(10)
             
-            SecureField("Password...", text: $viewModel.password)
-                .padding()
-                .background(Color.gray.opacity(0.4))
-                .cornerRadius(10)
+            AuthTitleComponent(firstLine: "Hello.", secondLine: "Welcome Champ")
             
-            Button  {
+            AuthEmailFieldComponent(email: $viewModel.email)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("PASSWORD")
+                    .tracking(2)
+                    .font(.headline)
+                    .foregroundColor(AppColors.authFieldHeader)
+                
+                SecureField("", text: $viewModel.password)
+                    .background(Color.clear)
+                    .foregroundColor(.white)
+                
+                Divider().background(Color.white)
+            }
+            .padding(.bottom, 60)
+            
+            
+            Button {
                 Auth.auth().signIn(withEmail: viewModel.email, password: viewModel.password) { result, error in
                     if error != nil {
                         print(error!.localizedDescription)
@@ -41,28 +52,69 @@ struct SignInEmailView: View, WithRootNavigationController {
                     .foregroundStyle(.white)
                     .frame(height: 55)
                     .frame(maxWidth: .infinity)
-                    .background(Color.blue)
+                    .background(AppColors.authButtonColor)
                     .cornerRadius(10)
+                    .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
             }
+            .padding(.bottom, 60)
+            
             
             Button  {
                 self.push(viewController: UIHostingController(rootView: RegisterView()), animated: true)
             } label: {
-                Text("Dont Have an account register NOWW!!")
-                    .font(.headline)
-                    .foregroundStyle(.white)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+                Text("Create Account")
             }
+            
             
             Spacer()
         }
         .padding()
+        .padding()
+        .background(AppColors.authPageBackground)
     }
 }
 
 #Preview {
     SignInEmailView()
+}
+
+struct AuthTitleComponent: View {
+    
+    let firstLine: String
+    let secondLine: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(firstLine)
+                .font(.custom("Helvetica-Bold", size: 35))
+                .foregroundStyle(.white)
+            Text(secondLine)
+                .font(.custom("Helvetica-Bold", size: 35))
+                .foregroundStyle(.white)
+            
+        }
+        .frame(maxWidth: .infinity, alignment: .topLeading)
+        .padding(.bottom, 120)
+    }
+}
+
+struct AuthEmailFieldComponent: View {
+    
+    @Binding var email: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("EMAIL")
+                .tracking(2)
+                .font(.headline)
+                .foregroundColor(AppColors.authFieldHeader)
+            
+            TextField("", text: $email)
+                .background(Color.clear)
+                .foregroundColor(.white)
+            
+            Divider().background(Color.white)
+        }
+        .padding(.bottom, 60)
+    }
 }
