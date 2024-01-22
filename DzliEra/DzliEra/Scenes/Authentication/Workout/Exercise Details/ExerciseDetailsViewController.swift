c4a196869e7bf402d4cafb22e5934619ef2111fb
Levan Loladze <levan.loladzew@gmail.com>
2024-01-22 21:31:47 +0400

    Add Instructions Label

--- DzliEra/DzliEra/Scenes/Authentication/Workout/Exercise Details/ExerciseDetailsViewController.swift
+++ DzliEra/DzliEra/Scenes/Authentication/Workout/Exercise Details/ExerciseDetailsViewController.swift
@@ -12,15 +12,6 @@ class ExerciseDetailsViewController: UIViewController {
     
     var exercise: Exercise?
     
-    private let nameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.boldSystemFont(ofSize: 20)
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }()
-    
     private let exerciseImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -29,11 +20,56 @@ class ExerciseDetailsViewController: UIViewController {
         return imageView
     }()
     
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Helvetica", size: 20.0)
+        label.numberOfLines = 0
+        label.textColor = .white
+        return label
+    }()
+    
+    private let targetLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Helvetica", size: 16.0)
+        label.numberOfLines = 0
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let secondaryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Helvetica", size: 16.0)
+        label.numberOfLines = 0
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let exerciseInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 6
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let instructionsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
         setupImageView()
+        setupStackView()
+        setupInstructionsLabel()
         updateUI()
     }
     
@@ -49,14 +85,51 @@ class ExerciseDetailsViewController: UIViewController {
         ])
     }
     
+    private func setupStackView() {
+        view.addSubview(exerciseInfoStackView)
+        
+        exerciseInfoStackView.addArrangedSubview(nameLabel)
+        exerciseInfoStackView.addArrangedSubview(targetLabel)
+        exerciseInfoStackView.addArrangedSubview(secondaryLabel)
+        
+        NSLayoutConstraint.activate([
+            exerciseInfoStackView.topAnchor.constraint(equalTo: exerciseImageView.bottomAnchor, constant: 16),
+            exerciseInfoStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            exerciseInfoStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func setupInstructionsLabel() {
+        view.addSubview(instructionsLabel)
+
+        NSLayoutConstraint.activate([
+            instructionsLabel.topAnchor.constraint(equalTo: exerciseInfoStackView.bottomAnchor, constant: 24),
+            instructionsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            instructionsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+    }
+    
     private func updateUI() {
         guard let exercise = exercise, let imageURL = URL(string: exercise.gifURL) else {
             return
         }
+        
+        let secondaryMusclesString = exercise.secondaryMuscles.joined(separator: ", ")
+    
+        let numberedInstructions = exercise.instructions.enumerated().map { (index, instruction) in
+            return "\(index + 1). \(instruction)"
+        }
+        
+        let instructionsString = numberedInstructions.joined(separator: "\n\n")
+
+        
         nameLabel.text = exercise.name
-        
+        targetLabel.text = "Target: " + exercise.target
+        secondaryLabel.text = "Secondary: " + secondaryMusclesString
         exerciseImageView.image = UIImage.gifImageWithName("workout")
-        
+        instructionsLabel.text = "Instructions:\n\n" + instructionsString
+
         DispatchQueue.global().async {
             if let imageData = try? Data(contentsOf: imageURL) {
                 let image = UIImage.gifImageWithData(imageData)
