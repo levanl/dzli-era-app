a06f20cced634ed2d495a2bcf2257aea87af2053
Levan Loladze <levan.loladzew@gmail.com>
2024-01-24 19:36:46 +0400

    Add workoutInfoStack and workoutInfoCounterStack

--- DzliEra/DzliEra/Scenes/Authentication/Workout/Started Routine/StartedRoutineViewController.swift
+++ DzliEra/DzliEra/Scenes/Authentication/Workout/Started Routine/StartedRoutineViewController.swift
@@ -11,42 +11,148 @@ class StartedRoutineViewController: UIViewController {
     
     var routine: Routine?
     var timer: Timer?
-    var elapsedTime: TimeInterval = 0
+    var elapsedTime: Int = 0
+    
+    private let durationLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Duration"
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let volumeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Volume"
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let setsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Sets"
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let workoutInfoStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
     
     private let timerLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "0 KG"
         label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
         return label
+    }()
+    
+    private let kgLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "0"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let setCounterLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "0"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let workoutInfoCounterStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 255/255, green: 253/255, blue: 208/255, alpha: 1.0) // Cream color
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        setupTimerLabel()
         startTimer()
+        setupWorkoutInfoStack()
+        setupWorkoutInfoCounterStack()
     }
     
     deinit {
         stopTimer()
         print("aiufuisa")
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         stopTimer()
     }
-
     
-    private func setupTimerLabel() {
-        view.addSubview(timerLabel)
+    private func setupWorkoutInfoStack() {
+        view.addSubview(workoutInfoStack)
+        
+        workoutInfoStack.addArrangedSubview(durationLabel)
+        workoutInfoStack.addArrangedSubview(volumeLabel)
+        workoutInfoStack.addArrangedSubview(setsLabel)
         
         NSLayoutConstraint.activate([
-            timerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            timerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            timerLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            workoutInfoStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            workoutInfoStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            workoutInfoStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
+        
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.addSubview(separatorView)
+
+            // Set up constraints for the separatorView
+            NSLayoutConstraint.activate([
+                separatorView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 10),
+                separatorView.leadingAnchor.constraint(equalTo: navigationBar.leadingAnchor),
+                separatorView.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor),
+                separatorView.heightAnchor.constraint(equalToConstant: 1) // Adjust the height as needed
+            ])
+        }
+    }
+    
+    private func setupWorkoutInfoCounterStack() {
+        view.addSubview(workoutInfoCounterStack)
+        
+        workoutInfoCounterStack.addArrangedSubview(timerLabel)
+        workoutInfoCounterStack.addArrangedSubview(kgLabel)
+        workoutInfoCounterStack.addArrangedSubview(setCounterLabel)
+        
+        NSLayoutConstraint.activate([
+            workoutInfoCounterStack.topAnchor.constraint(equalTo: workoutInfoStack.bottomAnchor, constant: 8),
+            workoutInfoCounterStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            workoutInfoCounterStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
     }
     
     func startTimer() {
@@ -62,15 +168,20 @@ class StartedRoutineViewController: UIViewController {
     
     @objc func updateTimer() {
         elapsedTime += 1
-        updateUI()
+        updateTimerLabel()
     }
     
-    private func updateUI() {
-        let minutes = Int(elapsedTime) / 60
-        let seconds = Int(elapsedTime) % 60
+    private func updateTimerLabel() {
+        let minutes = elapsedTime / 60
+        let seconds = elapsedTime % 60
         
-        let formattedTime = String(format: "%02d:%02d", minutes, seconds)
-        timerLabel.text = "Time: \(formattedTime)"
-        print(elapsedTime)
+        let timerText: String
+        
+        if minutes > 0 {
+            timerText = String(format: "%dmin %02ds", minutes, seconds)
+        } else {
+            timerText = String(format: "%ds", seconds)
+        }
+        timerLabel.text = timerText
     }
 }
