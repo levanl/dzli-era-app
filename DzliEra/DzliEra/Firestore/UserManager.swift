ef4715f92906e22d48beace2be9bec03d6eeadae
Levan Loladze <levan.loladzew@gmail.com>
2024-01-29 14:25:57 +0400

    Refactor getUser fucntion

--- DzliEra/DzliEra/Firestore/UserManager.swift
+++ DzliEra/DzliEra/Firestore/UserManager.swift
@@ -33,8 +33,18 @@ final class UserManager {
         return encoder
     }()
     
+    private let decoder: Firestore.Decoder = {
+        let decoder = Firestore.Decoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
     func createNewUser(user: DBUser) async throws {
         try userDocument(userId: user.userId).setData(from: user, merge: false, encoder: encoder)
+    }
+    
+    func getUser(userId: String) async throws -> DBUser {
+        try await userDocument(userId: userId).getDocument(as: DBUser.self, decoder: decoder)
     }
     
 //    func createNewUser(auth: AuthDataResultModel) async throws {
@@ -56,17 +66,17 @@ final class UserManager {
 //        try await userDocument(userId: auth.uid).setData(userData, merge: false)
 //    }
     
-    func getUser(userId: String) async throws -> DBUser {
-        let snapshot = try await userDocument(userId: userId).getDocument()
-        
-        guard let data = snapshot.data(),  let userId = data["user_id"] as? String else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let email = data["email"] as? String
-        let photoUrl = data["photo_url"] as? String
-        let dateCreated = data["date_created"] as? Date
-        
-        return DBUser(userId: userId, email: email, photoUrl: photoUrl, dateCreated: dateCreated)
-    }
+//    func getUser(userId: String) async throws -> DBUser {
+//        let snapshot = try await userDocument(userId: userId).getDocument()
+//        
+//        guard let data = snapshot.data(),  let userId = data["user_id"] as? String else {
+//            throw URLError(.badServerResponse)
+//        }
+//        
+//        let email = data["email"] as? String
+//        let photoUrl = data["photo_url"] as? String
+//        let dateCreated = data["date_created"] as? Date
+//        
+//        return DBUser(userId: userId, email: email, photoUrl: photoUrl, dateCreated: dateCreated)
+//    }
 }
