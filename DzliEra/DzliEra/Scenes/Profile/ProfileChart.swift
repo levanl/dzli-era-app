cb0bb227b6b975e7ee9402d82212e39194c01c5f
Levan Loladze <levan.loladzew@gmail.com>
2024-02-04 20:14:17 +0400

    Add Image and email display on ProfileViewController

---
+++ DzliEra/DzliEra/Scenes/Profile/ProfileChart.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileChart.swift
+//  DzliEra
+//
+//  Created by Levan Loladze on 04.02.24.
+//
+
+import SwiftUI
+import Charts
+
+
+struct ProfileChart: View {
+    var body: some View {
+        
+        
+        
+    }
+}
+
+#Preview {
+    ProfileChart()
+}
--- DzliEra/DzliEra/Scenes/Profile/ProfileViewController.swift
+++ DzliEra/DzliEra/Scenes/Profile/ProfileViewController.swift
@@ -20,6 +20,24 @@ class ProfileViewController: UIViewController {
         return label
     }()
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let profileStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,6 +48,7 @@ class ProfileViewController: UIViewController {
                 print("User loaded: \(self.user?.email ?? "Unknown email")")
                 DispatchQueue.main.async {
                     self.profileLabel.text = self.user?.email
+                    self.loadProfileImage()
                 }
             } catch {
                 print("Error loading user: \(error)")
@@ -37,20 +56,31 @@ class ProfileViewController: UIViewController {
         }
         
         view.backgroundColor = .black
-        setupProfileLabel()
+        setupProfileStackView()
     }
     
     
-    private func setupProfileLabel() {
-        view.addSubview(profileLabel)
+    private func setupProfileStackView() {
+        
+        view.addSubview(profileStackView)
+        
+        profileStackView.addArrangedSubview(profileImageView)
+        profileStackView.addArrangedSubview(profileLabel)
         
         NSLayoutConstraint.activate([
-            profileLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
-            profileLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            profileLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            profileStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            profileStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            profileStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            profileImageView.widthAnchor.constraint(lessThanOrEqualToConstant: 70),
+            profileImageView.heightAnchor.constraint(lessThanOrEqualToConstant: 70)
         ])
+        
     }
     
+    private func loadProfileImage() {
+            self.profileImageView.image = UIImage(named: "onboarding1")
+        }
+   
     
     
 }
