49b3640c1a8cf37449b8b19bb69b191c8dee9308
Levan Loladze <levan.loladzew@gmail.com>
2024-03-04 19:27:33 +0400

    Add Alert Extension

---
+++ DzliEra/DzliEra/Extensions/AlertExtension.swift
@@ -0,0 +1,19 @@
+//
+//  AlertExtension.swift
+//  DzliEra
+//
+//  Created by Levan Loladze on 04.03.24.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+//Show a basic alert
+func showAlert(alertText : String, alertMessage : String) {
+    let alert = UIAlertController(title: alertText, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+    alert.addAction(UIAlertAction(title: "Got it", style: UIAlertAction.Style.default, handler: nil))
+//Add more actions as you see fit
+self.present(alert, animated: true, completion: nil)
+  }
+}
