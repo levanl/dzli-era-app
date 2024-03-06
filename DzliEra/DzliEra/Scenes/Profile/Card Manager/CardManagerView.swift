175b71d4496c2d60a7f4e4fdba94f05b31ff79ec
Levan Loladze <levan.loladzew@gmail.com>
2024-03-06 16:21:44 +0400

    Add Spline 3D Card

--- DzliEra/DzliEra/Scenes/Profile/Card Manager/CardManagerView.swift
+++ DzliEra/DzliEra/Scenes/Profile/Card Manager/CardManagerView.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import SplineRuntime
 
 struct CardManagerView: View {
     var body: some View {
         VStack {
-            
+            ContentView()
         }
     }
 }
 
 #Preview {
     CardManagerView()
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        // fetching from cloud
+        let url = URL(string: "https://build.spline.design/NAVqDg9ozc3CafQZQkVU/scene.splineswift")!
+
+        // // fetching from local
+        // let url = Bundle.main.url(forResource: "scene", withExtension: "splineswift")!
+
+        try? SplineView(sceneFileURL: url).ignoresSafeArea(.all)
+    }
 }
