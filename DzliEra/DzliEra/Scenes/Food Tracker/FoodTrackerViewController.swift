33d56d98c073d2ba814b323d53d0f186a5bdee60
Levan Loladze <levan.loladzew@gmail.com>
2024-03-26 19:40:20 +0400

    Create FoodTrackerViewController

---
+++ DzliEra/DzliEra/Scenes/Food Tracker/FoodTrackerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FoodTrackerViewController.swift
+//  DzliEra
+//
+//  Created by Levan Loladze on 26.03.24.
+//
+
+import UIKit
+
+class FoodTrackerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
